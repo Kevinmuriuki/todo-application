62003a2a838aa2b94eb068a884895718c49082c6
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-05-25 09:20:53 +0300

    connect database and add read functionality

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+
+class Todo(db.model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 
 def index():
     return render_template('index.html', 
-    data=[
-        {'description': 'Todo 1'},
-        {'description': 'Todo 2'},
-        {'description': 'Todo 3'},
-        {'description': 'Todo 4'}
-    ])
+    data=Todo.query.all())
 
