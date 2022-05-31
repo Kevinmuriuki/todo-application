6fdb67ed926e22ca222b46139891816e4b842c88
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-05-31 08:36:53 +0300

    Add modifications

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import os
-from flask import Flask, render_template, request, redirect, jsonify
+from crypt import methods
+import json
+import os, sys
+from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
 
@@ -16,23 +18,51 @@ class Todo(db.Model):
     description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+        return f'<Todo {self.id} {self.description} {self.completed}>'
 
 db.create_all()
 
 #controllers
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}  
+  
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if  error:
+        abort(400)
+    else:            
+        print(jsonify('body', body))
+        return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
     return render_template('index.html', 
-    data=Todo.query.all())
+    todos=Todo.query.all())
 
--- static/app.js
+++ static/app.js
@@ -2,6 +2,32 @@ const form = document.getElementById('form');
 const description = document.getElementById('description');
 const todos = document.getElementById('todos')
 const errMessage = document.getElementById('error')
+const checkBoxes = document.querySelectorAll('.check-completed');
+
+console.log('todos', todos.innerText)
+
+checkBoxes.forEach((checkBox) => {
+    checkBox.onchange = (e) => {
+        const newCompleted = e.target.checked;
+        const todoId = e.target.dataset['id'];
+
+        fetch(`/todos/${todoId}/set-completed`, {
+            method: 'POST',
+            body: JSON.stringify({
+                'completed': newCompleted
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            errMessage.className = 'hidden';
+        })
+        .catch(() => {
+            errMessage.className = '';
+        })
+    }
+})
 
 form.onsubmit = e => {
     e.preventDefault();
